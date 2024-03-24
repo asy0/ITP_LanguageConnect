8fc8af57619e553586295dc54f240b3193b5f99f
Zeliha Vural <zeliha@outlook.com>
2024-03-24 23:02:29 +0100

    update

---
+++ src/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+        <link rel="stylesheet" href="root/css/style.css">
+        <title>LanguageConnect</title>
+    </head>
+
+    <body>
+     <header>
+        <!--- Navbar --->
+         <?php include './root/frontend/navbar.php'; ?>
+     </header>
+
+      <div class="container">
+         <br><br><br><div class="half-page-background">
+                    </div>
+
+         <div class="row justify-content-end">
+             <br/><div class="col-md-8">
+                 <br/><h1>Willkommen bei LanguageConnect!</h1>
+                 <p class="box-shad"> Herzlich willkommen bei LanguageConnect – Ihrem Tor zur Welt der Sprachen!
+                     Wir sind eine führende Sprachschule, die sich der Bereicherung des Lebens durch Sprachen
+                     verschrieben hat. Egal, ob Sie Ihre Karrierechancen erweitern, reisen oder
+                     einfach nur eine neue Kultur entdecken möchten, wir sind hier, um Sie auf Ihrer Sprachreise
+                     zu begleiten.
+                 </p>
+             </div>
+
+             <div class="container2">
+                 <h2><b>Unsere Mission</b></h2>
+                 <p>Unsere Mission bei LanguageConnect ist es, unseren Schülern ein einzigartiges und
+                     bereicherndes Sprachlernerlebnis zu bieten. Wir glauben daran, dass Sprachen nicht
+                     nur ein Kommunikationswerkzeug sind, sondern auch eine Brücke zu neuen Erfahrungen und
+                     Perspektiven darstellen. Durch unsere engagierten Lehrer und innovative Lernmethoden helfen wir
+                     Ihnen dabei, Ihre Ziele zu erreichen und Ihr volles Potenzial zu entfalten.</p>
+
+                 <h2>Unser Angebot</h2>
+                 <p>Bei LanguageConnect bieten wir eine breite Palette von Sprachkursen für alle Altersgruppen
+                     und Sprachniveaus an. Von Anfänger- bis Fortgeschrittenenkursen in verschiedenen
+                     Sprachen – darunter Englisch, Deutsch, Spanisch, Französisch und viele mehr – ist für
+                     jeden etwas dabei. Unsere flexiblen Kurszeiten und maßgeschneiderten Lernprogramme
+                     ermöglichen es Ihnen, Ihren Unterricht an Ihren individuellen Zeitplan anzupassen.</p>
+
+                 <h2>Warum LanguageConnect?</h2>
+                 <ul>
+                     <li>Erfahrene und qualifizierte Lehrkräfte</li>
+                     <li>Kleine Klassengrößen für eine persönliche Lernerfahrung</li>
+                     <li>Innovative Unterrichtsmethoden und modernste Lernressourcen</li>
+                     <li>Eine unterstützende und motivierende Lernumgebung</li>
+                     <li>Flexible Kurszeiten und maßgeschneiderte Programme</li>
+                 </ul>
+                 <div class="col-md-6">
+                     <img src="./root/img/photo7.png" alt="Bildbeschreibung">
+                 </div>
+
+                 <h2>Beginnen Sie Ihre Sprachreise noch heute</h2>
+                 <p>Egal, ob Sie eine neue Sprache lernen, Ihre Kenntnisse verbessern oder sich auf internationale
+                     Prüfungen vorbereiten möchten – bei LanguageConnect sind Sie genau richtig.
+                     Treten Sie noch heute unserer Sprachgemeinschaft bei und entdecken Sie die endlosen
+                     Möglichkeiten, die Ihnen die Beherrschung einer neuen Sprache eröffnet. Kontaktieren Sie uns,
+                     um mehr über unsere Kurse und Angebote zu erfahren oder vereinbaren Sie gleich eine kostenlose
+                     Probestunde!
+                 </p>
+             </div>
+         </div>
+      </div>
+
+
+     <!--- Footer --->
+     <?php include './root/frontend/footer.php'; ?>
+  </body>
+</html>
