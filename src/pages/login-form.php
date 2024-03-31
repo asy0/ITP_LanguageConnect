dfdc2788852dea134036cf078c00f58616a52147
Wendelin Windhager <148205166+ComicCed@users.noreply.github.com>
2024-03-31 16:14:41 +0200

    Login und Registrierungsseiten

---
+++ src/pages/login-form.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+<div class="container">
+
+    <?php if(isset($message)){
+        echo '<br>';
+        echo $message;
+        echo '<br>';
+    }
+    ?>
+
+    <h1>Anmeldung</h1><br><br>
+
+    <?php if (isset($failedAttempt)): ?>
+        <div class="alert alert-danger">
+            <i class="bi bi-exclamation-triangle"></i>
+            Username oder Passwort ist falsch. Bitte probieren Sie es erneut.
+        </div>
+    <?php endif; ?>
+
+
+    <form action="index.php?action=login" method="post">
+
+        <div class="form-group">
+            <label for="username">Username: </label>
+            <input type="text" id="username" name="username"
+                   class="form-control" required="required"/>
+
+        </div>
+
+        <div class="form-group">
+            <label for="password">Passwort </label>
+            <input type="password" id="password" name="password"
+                   class="form-control" required="required"/>
+
+        </div>
+        <br>
+        <input type="submit" class="btn btn-primary" value="Login">
+
+
+    </form>
+
+</div>
+
+
+
+</body>
+
+</html>
+
---
+++ src/pages/registrierung-form.php
@@ -0,0 +1,58 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<h1>Registrierung</h1><br><br>
+
+<?php if(isset($message)){
+echo '<br> </br>';
+echo $message;
+}
+?>
+
+        <form action="index.php?action=registrierung" method="post">
+
+        <div class="mb-3">
+            <label for="foa" class="form-label">Anrede:</label>
+            <input type="text" class="form-control" id="foa" placeholder="Anrede" name="foa">
+          </div>
+          
+          <div class="mb-3">
+            <label for="firstname" class="form-label">Vorname:</label>
+            <input type="text" class="form-control" id="firstname" placeholder="Vorname" name="firstname">
+          </div>
+    
+          <div class="mb-3">
+            <label for="lastname" class="form-label">Nachname:</label>
+            <input type="text" class="form-control" id="lastname" placeholder="Nachname" name="lastname">
+          </div>
+    
+          <div class="mb-3 mt-3">
+            <label for="email" class="form-label">E-Mail:</label>
+            <input type="email" class="form-control" id="email" placeholder="Email" name="email">
+          </div>
+    
+          <div class="mb-3">
+            <label for="username" class="form-label">Username:</label>
+            <input type="text" class="form-control" id="username" placeholder="Username" name="username">
+          </div>
+          
+          <div class="mb-3">
+            <label for="pwd" class="form-label">Passwort:</label>
+            <input type="password" class="form-control" id="pwd" placeholder="Passwort" name="pwd">
+          </div>
+    
+          <div class="mb-3">
+            <label for="pwdconfirm" class="form-label">Passwort bestätigen:</label>
+            <input type="password" class="form-control" id="pwdconfirm" placeholder="Passwort bestätigen" name="pwdconfirm">
+          </div>
+    
+        <button type="submit" class="btn btn-primary" value="registrieren">Registrierung abschließen</button>
+      </form>
+
+</body>
+
+
+</html>
