fdaefca48c011dc626b2dee127c1ec7e94a4dfa5
Zeliha Vural <zeliha@outlook.com>
2024-04-02 12:07:20 +0200

    impressum style&korrektur done

---
+++ src/root/frontend/impressum.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Impressum</title>
+</head>
+
+<header class="center">
+    <!--- Navbar --->
+    <?php include './navbar.php'; ?>
+</header>
+
+<body>
+ <div class="impressum-container center">
+    <h1 class="header-imp">IMPRESSUM</h1>
+
+    <p>
+        Diese Website wird betrieben
+        von der LanguageConnect Austria Gesellschaft (Handelskai 23, A-1200 Wien).
+    </p>
+
+    <div style="height: 30px;"></div>
+    <p class="left-align">LanguageConnect Austria Gesellschaft m.b.H.</p>
+    <h2 class="left-align">Postanschrift</h2>
+    <p class="left-align">LanguageConnect, Handelskai 23, A-1200 Wien </p>
+
+    <h2 class="left-align">Berufsrechtliche Regelungen</h2>
+    <p class="left-align">
+        • § 34 d Gewerbeordnung<br>
+        • §§ 59-68 VVG <br>
+        • VersVermV
+    </p>
+
+    <h2 class="left-align">Erlaubnisbefreiung nach § 34 d Abs. 3 GewO, Aufsichtsbehörde</h2>
+    <p class="left-align">IHK für Wien
+        Max-Muthgasse-Straße 2<br>
+        1990 Wien<br>
+        Österreich</p>
+
+    <h2 class="left-align">Firmenbuch</h2>
+    <p class="left-align">FN 385 d. LG Wien
+        <br>UID ATU3321444
+    </p>
+
+    <h2 class="left-align">Kontakt</h2>
+    <p class="left-align">
+        Kundeninformationscenter: +43 (0)800 -22 52 53<br>
+        Mo-Sa 08:00 bis 18:00 Uhr<br>
+        Sonntag und Feiertags geschlossen<br>
+        Telefon: +43(0)623 -73 33-0<br>
+        Fax: +43(0)662 -83 83-295<br>
+    </p>
+
+    <h2 class="left-align">Gewerberegister (https://www.gisa.gv.at/)</h2>
+    <p class="left-align">GISA-Zahl: 27506349
+        Erlaubnis nach § 137 GewO erteilt durch die Aufsichtsbehörde:
+        Berufsbezeichnung: Versicherungsvermittlung in der Form Versicherungsagent Republik Österreich, Republik Ungarn,
+        Hellenische Republik Griechenland, Republik Zypern</p>
+ </div>
+
+<!--- Footer --->
+    <footer class="center">
+        <?php include './footer.php'; ?>
+    </footer>
+</body>
+</html>
