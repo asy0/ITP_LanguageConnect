0ef6550cae350461f3f477ebbe751c0a1051c4ed
Zeliha Vural <zeliha@outlook.com>
2024-04-02 18:27:38 +0200

    some changes

--- src/root/frontend/hilfeseite.php
+++ src/root/frontend/hilfeseite.php
@@ -1,12 +1,16 @@
 <!DOCTYPE html>
-<html>
+<html lang="de">
 <head>
     <meta charset="UTF-8">
-    <title>Hilfe</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="../css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Hilfe</title>
 </head>
-<body>
+
+<body class="hilfebody">
 <header>
     <?php
     include 'navbar.php';
@@ -45,10 +49,10 @@
     <h4>10. Wie sicher sind meine Daten bei LanguageConnect?</h4>
     <p><em>Ihre Daten sind durch fortschrittliche Sicherheitsmaßnahmen geschützt und werden vertraulich behandelt.</em></p>
 </main>
-<footer>
-    <footer class="center">
+
+    <footer>
         <?php include './footer.php'; ?>
     </footer>
-</footer>
+
 </body>
 </html>
