b417c9f98a752d91f0ac787ac0ed709a521e4f9a
shehab <smail9423@gmail.com>
2024-04-15 18:16:46 +0200

    test4

---
+++ src/root/frontend/Tutorprofil.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Profil</title>
+</head>
+
+
+<body class="tutor-profile-body">
+<header>
+    <!--- Navbar --->
+    <?php include './navbar.php'; ?>
+</header>
+
+<main>
+<div class="profile-container">
+    <h1>Mein Profil</h1>
+    <div class="profile-info">
+        <p>Anrede: </p>
+        <p>Name: </p>
+        <p>Geschlecht: /p>
+        <p>Telefon: </p>
+        <p>E-Mail: </p>
+        <p>Stundenlohn: </p>
+        <p>Anzahl der Kurse:</p>
+        <p>Spezialisierungen:</p>
+        <p>Verfügbarkeit: </p>
+        <p>Sprachkenntnisse: </p>
+        <p>Bildungsabschluss: </p>
+
+    </div>
+</div>
+
+</main>
+
+<footer>
+    <!--- Footer --->
+    <?php include './footer.php'; ?>
+</footer>
+
+</body>
+</html>
+
