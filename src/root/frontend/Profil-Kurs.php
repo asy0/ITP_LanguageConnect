280e207fd48a3c4738d7fa534c0c7f4cc2ee741f
shehab <smail9423@gmail.com>
2024-04-16 21:40:11 +0200

    test4

---
+++ src/root/frontend/Profil-Kurs.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Profil</title>
+</head>
+
+
+<body>
+<header>
+    <!--- Navbar --->
+    <?php include './navbar.php'; ?>
+</header>
+
+<main>
+
+    <div class="buttonbox">
+    <a href="profile.html" class="button">Profilansicht</a>
+    <a href="course_list.html" class="button">Kursliste</a>
+    </div>
+</main>
+
+<footer>
+    <!--- Footer --->
+    <?php include './footer.php'; ?>
+</footer>
+
+</body>
+</html>
+
+
