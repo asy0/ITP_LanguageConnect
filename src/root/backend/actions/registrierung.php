594cb247940192e2153e75517b564ab1dbaaf237
Zeliha Vural <zeliha@outlook.com>
2024-04-30 21:20:24 +0200

    some changes

--- src/root/backend/actions/registrierung.php
+++ src/root/backend/actions/registrierung.php
@@ -1,28 +1,25 @@
 <?php
 $message = "";
-
-$benutzername = $_POST['username'];
-$anrede = $_POST['foa'];
+$username = $_POST['username'];
+$anrede = $_POST['anrede']; // hier war der Name im Formular "foa"
 $vorname = $_POST['firstname'];
 $nachname = $_POST['lastname'];
 $email = $_POST['email'];
 $password = $_POST['pwd'];
 $passwordconfirm = $_POST['pwdconfirm'];
 
-
-
 if(strlen($password) > 100 || strlen($password) < 8){
     $message = "Das Passwort muss mindestens 8 Zeichen und nicht über 100 Zeichen haben. Bitte versuche es erneut.";
     header("Location: ../../frontend/registrierung-form.php");
+    exit();
 }
 
 // Überprüfen, ob die Passwörter übereinstimmen
-
 if ($password !== $passwordconfirm) {
-    $message = "Die Passwörter stimmen nicht überein. Bitte versuche es erneut.";
-    header("Location: ../../frontend/registrierung-form.php");
+    $message = 'Die Passwörter stimmen nicht überein.';
+    header('Location: ../../frontend/registrierung-form.php?errorReg=' . urlencode($message));
+    exit();
 } else {
-
     include 'C:\xampp\htdocs\ITP_LanguageConnect\src\root\backend\db_connect.php';
     $db = getDBConnection();
 
@@ -36,24 +33,21 @@ if ($password !== $passwordconfirm) {
     if ($count > 0) {
         $message = "Benutzername bereits vorhanden. Bitte wähle einen anderen Benutzernamen.";
         header("Location: ../../frontend/registrierung-form.php");
+        exit();
     } else {
-
         $hashedpassword = password_hash($password, PASSWORD_DEFAULT);
-        $stmt = $db->prepare("INSERT INTO users (Anrede, Vorname, Nachname, EMail, Username, Passwort, admin) VALUES (?, ?, ?, ?, ?, ?, 0)");
-        $stmt->bind_param("ssssss", $anrede, $vorname, $nachname, $email, $benutzername, $hashedpassword);
+        $stmt = $db->prepare("INSERT INTO users (anrede, firstname, lastname, email, username, password, role) VALUES (?, ?, ?, ?, ?, ?,?)");
+        $stmt->bind_param("sssssss", $anrede, $vorname, $nachname, $email, $username, $hashedpassword, $role);
 
-        if ($stmt) {
-            $stmt->execute();
+        if ($stmt->execute()) {
             $stmt->close();
+            $db->close();
+            // Erfolgreiche Registrierung, weiterleiten zur userlogged Seite
+            header("Location: ./backend/userlogged.php");
+            exit();
         } else {
-            die('Fehler beim Vorbereiten des Statements: ' . $db->error);
+            die('Fehler beim Ausführen des Statements: ' . $stmt->error);
         }
-        $message = "Registrierung erfolgreich! Sie können sich nun einloggen!";
-        header("Location: ../../frontend/login-form.php");
     }
-
-    $db->close();
 }
-
-
 
