10c6a199497d72e52cdb942cc3d94c5221717a48
Zeliha Vural <zelihavural@outlook.com>
2024-05-30 18:54:51 +0200

    stylen

--- src/root/frontend/kursliste.php
+++ src/root/frontend/kursliste.php
@@ -7,9 +7,48 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link rel="stylesheet" href="../css/style.css">
+    <style>
+        .buttonbox {
+            display: flex;
+            align-items: center;
+            margin-bottom: 20px;
+            padding: 20px;
+            background-color: #ffffff;
+            border-radius: 8px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+        }
+        .profile-pic {
+            margin-right: 20px;
+        }
+        .profile-pic img {
+            width: 250px;
+            height: 250px;
+            object-fit: cover;
+            border-radius: 50%;
+        }
+        .info {
+            display: flex;
+            flex-direction: column;
+        }
+        .info div {
+            margin-bottom: 10px;
+        }
+        body {
+            background-color: #f0f2f5;
+        }
+        main {
+            padding: 20px;
+        }
+        .welcome-heading {
+            text-align: center;
+            text-decoration: underline;
+            font-size: 3.5rem;
+            font-weight: bold;
+
+        }
+    </style>
     <title>Profil</title>
 </head>
-
 
 <body>
 <header>
@@ -18,12 +57,11 @@
 </header>
 
 <main>
-    <h1 class="welcome-heading">Kursliste</h1>
+    <br><br><h1 class="welcome-heading">Kursliste</h1><br>
     <div class="buttonbox">
         <div class="profile-pic">
             <!-- Hier könnte Ihr Bild eingefügt werden -->
             <img src="https://www.oefb.at/oefb2/images/1278650591628556536_6e8a657959df7155a926-1,0-320x320.png" alt="Beschreibung des Bildes">
-
         </div>
         <div class="info">
             <div><strong>Name:</strong> Max Mustermann</div>
@@ -36,7 +74,6 @@
         <div class="profile-pic">
             <!-- Hier könnte Ihr Bild eingefügt werden -->
             <img src="https://www.oefb.at/oefb2/images/1278650591628556536_6e8a657959df7155a926-1,0-320x320.png" alt="Beschreibung des Bildes">
-
         </div>
         <div class="info">
             <div><strong>Name:</strong> Max Mustermann</div>
@@ -49,7 +86,6 @@
         <div class="profile-pic">
             <!-- Hier könnte Ihr Bild eingefügt werden -->
             <img src="https://www.oefb.at/oefb2/images/1278650591628556536_6e8a657959df7155a926-1,0-320x320.png" alt="Beschreibung des Bildes">
-
         </div>
         <div class="info">
             <div><strong>Name:</strong> Max Mustermann</div>
@@ -67,5 +103,3 @@
 
 </body>
 </html>
-
-
