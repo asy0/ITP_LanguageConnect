162e904960a0c79f46094124ce4f914917042609
Zeliha Vural <zelihavural@outlook.com>
2024-05-30 18:55:47 +0200

    admin hinzugefügt+refactoring

--- src/root/backend/actions/login.php
+++ src/root/backend/actions/login.php
@@ -9,39 +9,31 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     include 'C:\xampp\htdocs\ITP_LanguageConnect\src\root\backend\db_connect.php';
 
     $db = getDBConnection();
-
     $sql = "SELECT * FROM users WHERE username = ?";
     $stmt = $db->prepare($sql);
     $stmt->bind_param("s", $username);
     $stmt->execute();
 
     $result = $stmt->get_result();
-
-    // Überprüfen, ob ein Datensatz gefunden wurde
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
-
-        // Überprüfen, ob der Account inaktiv ist
         if ($row["status"] == 0) {
             $message = "Profil inaktiv, bitte wenden Sie sich an einen Administrator.";
         } else {
-            // Account ist aktiv, weiter mit Passwortüberprüfung
             $hashedPasswordInDB = $row["password"];
-
             if (password_verify($password, $hashedPasswordInDB)) {
-                // Variable (isAdmin) wird gesetzt, wenn der Benutzer ein Admin ist
-                if ($row["role"] == "Admin") {
-                    $_SESSION['isAdmin'] = true;
-                }
-                if ($row["role"] == "Tutor"){
-                    $_SESSION['isTutor'] = true;
-                }
-
-                $message = "Anmeldung erfolgreich!";
                 $_SESSION['logged_in'] = true;
                 $_SESSION['username'] = $username;
 
-                header("Location: ../../backend/userlogged.php");
+                if ($row["role"] == "Admin") {
+                    $_SESSION['isAdmin'] = true;
+                    header("Location: ../../admin/adminindex.php");
+                } elseif ($row["role"] == "Tutor") {
+                    $_SESSION['isTutor'] = true;
+                    header("Location: ../../backend/userlogged.php");
+                } else {
+                    header("Location: ../../backend/userlogged.php");
+                }
                 exit();
             } else {
                 $message = "Anmeldung fehlgeschlagen. Bitte überprüfen Sie Benutzername und Passwort.";
@@ -51,7 +43,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $message = "Benutzername nicht registriert. Bitte registrieren Sie sich zuerst.";
     }
 
-    // Statement und Db werden geschlossen
     $stmt->close();
     $db->close();
 }
