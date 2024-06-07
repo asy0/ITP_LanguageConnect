b5b8a07ee474b8b9ecdc1d00f2108adf560422ef
Wendelin Windhager <wendelin.windhager@gmail.com>
2024-06-07 20:13:57 +0200

    Fix in Mein_Profil
    Falsches Weiterleiten beim Button "Meine Kurse" gefixt

--- src/root/frontend/myprofil.php
+++ src/root/frontend/myprofil.php
@@ -66,7 +66,7 @@
         </form>
 
         <div class="form-group text-center mt-4">
-            <button onclick="window.location.href = 'kurserstellen.php';" class="btn btn-info">Zu meinen Kursen</button>
+            <button onclick="window.location.href = 'meine-kurse.php';" class="btn btn-info">Zu meinen Kursen</button>
         </div>
     </div>
 </div>
